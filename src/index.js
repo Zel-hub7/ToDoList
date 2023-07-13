35a832c0f087c26453b60ca3a0ec863f1e3a0d71
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-13 05:12:00 -0700

    Modify file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,67 @@
 import "./style.css";
 
-const tasks = [
-  {
-    description: 'Complete assignment',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: "Buy groceries",
-    completed: true,
-    index: 1,
-  },
-  {
-    description: "Call a friend",
-    completed: false,
-    index: 2,
-  },
-];
+const tasks = [];
 
+function populateTodoList() {
+  const todoList = document.getElementById('todo-list');
 
-const  populateTodoList =() => {
-    const todoList = document.getElementById('todo-list');
-    tasks.sort((a,b) => a.index - b.index);
-    tasks.forEach(task => {
-      const listItem = document.createElement('li');
-      listItem.textContent = task.description;
-  
-      if (task.completed) {
-        listItem.classList.add('completed');
+  todoList.innerHTML = '';
+
+  tasks.forEach(task => {
+    const listItem = document.createElement('li');
+    listItem.textContent = task.description;
+    listItem.className = 'todo-item'; // Add class for positioning
+
+    if (task.completed) {
+      listItem.classList.add('completed');
+    }
+
+    const menuIcon = document.createElement('span');
+    menuIcon.className = 'menu-icon';
+    menuIcon.innerHTML = '&#8942;';
+    menuIcon.addEventListener('click', () => {
+      if (menuIcon.classList.contains('delete-icon')) {
+        removeTask(task);
+      } else {
+        menuIcon.classList.add('delete-icon');
       }
-  
-      todoList.appendChild(listItem);
     });
+
+    listItem.appendChild(menuIcon);
+    todoList.appendChild(listItem);
+  });
+}
+
+function addTask(event) {
+  if (event.key === 'Enter') {
+    const newTaskInput = document.getElementById('new-task');
+    const newTaskDescription = newTaskInput.value.trim();
+
+    if (newTaskDescription !== '') {
+      const newTask = {
+        description: newTaskDescription,
+        completed: false,
+        index: tasks.length,
+      };
+
+      tasks.push(newTask);
+      populateTodoList();
+      newTaskInput.value = '';
+    }
   }
-  document.addEventListener('DOMContentLoaded', populateTodoList);
+}
+
+function removeTask(task) {
+  const taskIndex = tasks.indexOf(task);
+  if (taskIndex > -1) {
+    tasks.splice(taskIndex, 1);
+    populateTodoList();
+  }
+}
+
+// Event listener for the "Enter" key
+const newTaskInput = document.getElementById('new-task');
+newTaskInput.addEventListener('keydown', addTask);
+
+// Call the function to populate the initial To-Do list
+populateTodoList();
