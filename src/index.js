fc233d849604dbfaf356ca8d3d410bbb7da83d8c
zel-hub7 <zelalemyohannes62@gmail.com>
2023-07-13 08:04:34 -0700

    eslint error fixed

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
 import './style.css';
+
 const tasks = [
-    {
-      description: "Task 1",
-      completed: false,
-      index: 0,
-    },
-    {
-      description: "Task 2",
-      completed: true,
-      index: 1,
-    },
-    {
-      description: "Task 3",
-      completed: false,
-      index: 2,
-    },
-  ];
-  tasks.sort((a,b) => a.index - b.index);
-  function populateTodoList() {
-    const todoList = document.getElementById('todo-list');
-  
-    for(let i = 0; i <tasks.length; i++) {
-        const { index, description, completed } = tasks[i];
-        todoList.innerHTML += `
+  {
+    description: 'Task 1',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Task 2',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Task 3',
+    completed: false,
+    index: 2,
+  },
+];
+tasks.sort((a, b) => a.index - b.index);
+function populateTodoList() {
+  const todoList = document.getElementById('todo-list');
+
+  for (let i = 0; i < tasks.length; i += 1) {
+    const { index, description, completed } = tasks[i];
+    todoList.innerHTML += `
         
        
         <li id="L${index}" class ="common">
         <div class = "list_container">
         <div class = "list2">
         <input for ="P${index}" id="${index}" type="checkbox" ${
-      completed && 'checked'
-    }  class ="checkbox">
+  completed && 'checked'
+}  class ="checkbox">
         <p id ="P${index}" class="li-p">${description}</p>
         </div>
         <button id="edit-remove${index}"  class="btn dots list-item">
@@ -39,12 +40,8 @@ const tasks = [
         </li>
         </div>
       `;
-
-    }
   }
-  
-  
-  // Call the function to populate the list on page load
-  populateTodoList();
-  
-  
+}
+
+// Call the function to populate the list on page load
+populateTodoList();
